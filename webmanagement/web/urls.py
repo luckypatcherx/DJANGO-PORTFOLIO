158cfbf970a5658d4d24a2561b877fcbdd6bb2ac
luckypatcherx <59965177+luckypatcherx@users.noreply.github.com>
2023-05-01 05:49:53 -0700

    Update urls.py

--- webmanagement/web/urls.py
+++ webmanagement/web/urls.py
@@ -1,6 +1,6 @@
 """webmanagement URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes  to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
